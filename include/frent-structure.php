ab997ca6b4a910ab0898bc0658c8c3dd5dd24f55
tarunyaduvanshi <tarunyaduvanshi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-18 20:02:08 +0000

    revert structure
    git-svn-id: https://plugins.svn.wordpress.org/super-advanced-posts/trunk@1115501 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/frent-structure.php
+++ include/frent-structure.php
@@ -1,26 +1,8 @@
 <?php 
-if($post_typess == 'post'){ 
-$the_query = new WP_Query( array( 
-  'post_type' => $post_typess,
-  'orderby' => $orderby,
- 'category_name' => $taxcatin, 
-  'order' => $order,
-  'posts_per_page' => $showposts)); 
-}else{
-$the_query = new WP_Query( array(
-  'post_type' => $post_typess,
-  'numberposts' => $showposts,
-  'tax_query' => array(
-    array(
-      'taxonomy' => $taxtermin,
-      'terms' => $taxcatin, 
-        )
-  )
-));
-} 
+query_posts('orderby='.$orderby.'&order='.$order.'&post_type='.$post_typess.'&showposts='.$showposts.''); 
+
 echo '<ul >';
-while ( $the_query->have_posts() ) :
-   $the_query->the_post();
+while (have_posts()) : the_post(); 
  echo '<li class="post-item">';
 ?>
 <a class="post-title" href="<?php echo get_permalink(); ?>">
